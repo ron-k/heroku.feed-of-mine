7c24b7ffe803eb625c51ce5059dad9db91274425
Ron Kassay <ronk@celltick.com>
2018-06-23 21:17:09 +0300

    raised logging

--- src/main/java/com/rndysoft/linkofmy/handlers/HandlerRss.java
+++ src/main/java/com/rndysoft/linkofmy/handlers/HandlerRss.java
@@ -26,7 +26,7 @@ public class HandlerRss implements Handler {
             List<FeedItem> items = getFeedItems(ctx);
             Map<String, FeedModel> out = new HashMap<>();
             out.put("feed", new FeedModel(items));
-            LOGGER.debug("handle: attributes=%s", out);
+            LOGGER.error("handle: attributes=%s", out);
             return out;
 
         }).onError(throwable -> {
