4a8509757bbb55c51d6db6b2599f77a5654d8024
Ron Kassay <ronk@celltick.com>
2018-06-23 21:05:37 +0300

    rc1

---
+++ src/main/java/com/rndysoft/linkofmy/db/Schema.java
@@ -0,0 +1,7 @@
+package com.rndysoft.linkofmy.db;
+
+public class Schema {
+    public static final String TABLE_LINKS = "links";
+    public static final String COLUMN_LINK = "link";
+    public static final String COLUMN_TIME_ADDED = "timeAdded";
+}
