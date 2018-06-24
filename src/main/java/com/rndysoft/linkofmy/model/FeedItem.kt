d7e9cec4b098d103184f30ad451b4c18fe17cd30
Ron Kassay <ronk@celltick.com>
2018-06-24 23:47:20 +0300

    rewrote model entities using Kotlin

---
+++ src/main/java/com/rndysoft/linkofmy/model/FeedItem.kt
@@ -0,0 +1,5 @@
+package com.rndysoft.linkofmy.model
+
+import java.util.*
+
+data class FeedItem(val link: String, val addedTime: Date)
---
+++ src/main/java/com/rndysoft/linkofmy/model/FeedModel.kt
@@ -0,0 +1,3 @@
+package com.rndysoft.linkofmy.model
+
+data class FeedModel(val items: List<FeedItem>)
